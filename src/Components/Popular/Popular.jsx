e2013d0bede0211134a73fca90b49e3038708405
rawooldipesh <rawooldipesh0@gmail.com>
2025-06-16 03:17:10 +0530

    changed localhost to  backend live final .

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import './Popular.css'
-import Item from '../Item/Item'
+import React, { useEffect, useState } from 'react';
+import './Popular.css';
+import Item from '../Item/Item';
 
 const Popular = () => {
   const [popularProducts, setPopularProducts] = useState([]);
@@ -17,17 +17,15 @@ const Popular = () => {
       <h1>OUR SPECIALS</h1>
       <hr />
       <div className="popular-item">
-        {popularProducts.map((item, i) => {
-          return (
-            <Item
-              key={i}
-              id={item.id}
-              name={item.name}
-              image={`${BASE_URL}/uploads/${item.image}`} // if image is stored as filename
-              price={item.price}
-            />
-          );
-        })}
+        {popularProducts.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image} // ✅ Use image directly
+            price={item.price}
+          />
+        ))}
       </div>
     </div>
   );
