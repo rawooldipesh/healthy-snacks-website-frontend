1df59cb8be15f8a8923310d1d9a90e1e7056e05a
rawooldipesh <rawooldipesh0@gmail.com>
2025-06-16 01:12:40 +0530

    check

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import './Popular.css'
-// import data_product from '../Assets/data'
 import Item from '../Item/Item'
 
 const Popular = () => {
+  const [popularProducts, setPopularProducts] = useState([]);
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-  const [popularProducts,setPopularProducts] = useState([]);
-    useEffect(()=>{
-      fetch('http://localhost:4000/popular')
-      .then((response)=>response.json())
-      .then((data)=>setPopularProducts(data));
-    })
+  useEffect(() => {
+    fetch(`${BASE_URL}/popular`)
+      .then((response) => response.json())
+      .then((data) => setPopularProducts(data));
+  }, [BASE_URL]);
 
   return (
     <div className='popular'>
-        <h1>OUR SPECIALS</h1>
-        <hr/>
-        <div className="popular-item">
-            {popularProducts.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} price={item.price}/>
-            })}
-        </div>
+      <h1>OUR SPECIALS</h1>
+      <hr />
+      <div className="popular-item">
+        {popularProducts.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={`${BASE_URL}/uploads/${item.image}`} // if image is stored as filename
+              price={item.price}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Popular
+export default Popular;
